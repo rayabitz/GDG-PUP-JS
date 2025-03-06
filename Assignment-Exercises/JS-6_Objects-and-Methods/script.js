4c2df0bd7c4eee505a3e655c6f611fa911d4e460
rayabitz <mayvonnenviernes@iskolarngbayan.pup.edu.ph>
2025-03-06 12:22:31 +0800

    Update script.js

--- Assignment-Exercises/JS-6_Objects-and-Methods/script.js
+++ Assignment-Exercises/JS-6_Objects-and-Methods/script.js
@@ -1,8 +1,17 @@
 // Step 1: Create a car object
 
+let car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2021,
+};
 
 // Step 2: Create a function to describe the car
 
+function getDescription(carObject) {
+  return `This car is a ${carObject.year} ${carObject.make} ${carObject.model}.`;
+}
 
 // Step 3: Call the function and display the result
 
+console.log(getDescription(car));
