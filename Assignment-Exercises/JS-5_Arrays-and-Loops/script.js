d94927515413f75eeb86dfa168e2c14e3561fedf
rayabitz <mayvonnenviernes@iskolarngbayan.pup.edu.ph>
2025-03-06 12:17:58 +0800

    Update script.js

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,13 @@
+//No code is provided in this file, so it may differ from the comments and instructions.
+
+let favoriteMovies = [
+  "Interstellar",
+  "Avengers: Infinity War",
+  "How to Train Your Dragon",
+  "Guardians of the Galaxy",
+  "Howl's Moving Castle",
+];
+
+for (let i = 0; i < favoriteMovies.length; i++) {
+  console.log(favoriteMovies[i]);
+}
